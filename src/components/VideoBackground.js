23593e07c55f7c284d90cd155f1ebc2e474e0141
Shubhamp <shubhamphadtare6@gmail.com>
2023-09-28 19:31:34 +0530

    latest commit

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -8,9 +8,9 @@ const VideoBackground = ({movieId}) => {
     useMovieTrailer(movieId);
 
     return(
-        <div className="w-screen" >
+        <div className="w-[98vw]" >
             <iframe
-             className="w-screen aspect-video" 
+             className="w-[98vw] aspect-video" 
             src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
         </div>
     );
